5d7b8c32c7d9c75212d27d61fcba3eb2f033c9ed
gustavovizcarrabenavides <77975543+gustavovizcarrabenavides@users.noreply.github.com>
2022-07-01 15:39:34 -0500

    Implementación del menú

---
+++ escapa-del-covid/src/scenes/Play.js
@@ -0,0 +1,63 @@
+import RedBoy from "../Player/RedBoy";
+import Virus from "../Objects/Virus";
+import RedCrossItem from "../Objects/RedCrossItem";
+
+class Play extends Phaser.Scene {
+    constructor () {
+        super({
+            key: 'Play'
+        })
+    }
+
+    init () {
+        this.scene.launch('UI');
+    }
+
+    preload () {
+    }
+
+    create () {
+        this.add.image(0, 0, 'background').setOrigin(0);
+        //bloque fisico
+        this.wall_floor = this.physics.add.staticGroup();
+        this.wall_floor.create(0,0,'wall').setOrigin(0);
+        this.wall_floor.create(this.scale.width, 0, 'wall').setOrigin(1, 0).setFlipX(true);
+        this.wall_floor.create(0, this.scale.height, 'floor').setOrigin(0, 1);
+        this.wall_floor.refresh();
+        this.wall_floor.getChildren()[2].setOffset(0, 15);
+
+        // Virus group
+        this.virusGroup = new Virus({
+            physicsWorld: this.physics.world,
+            scene: this
+        });
+        this.itemsGroup = new RedCrossItem({
+            physicsWorld: this.physics.world,
+            scene: this
+        });
+
+        // Player
+        this.redBoy = new RedBoy({
+            scene: this,
+            x: 100,
+            y: 100
+        });
+        this.physics.add.collider([this.redBoy, this.virusGroup], this.wall_floor);
+        this.physics.add.overlap(this.redBoy, this.virusGroup, () => {
+            this.redBoy.virusCollision();
+        });
+        this.physics.add.overlap(this.itemsGroup, this.redBoy, () => {
+            this.registry.events.emit('update_points');
+            this.itemsGroup.destroyItem();
+            this.virusGroup.addVirus();
+        });
+    }
+
+    update () {
+        this.redBoy.update();
+        this.virusGroup.update();
+    }
+
+}
+
+export default Play;
